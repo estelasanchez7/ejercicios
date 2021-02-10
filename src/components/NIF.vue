28e77f0961f7af02aaee280fd9e7d058f0a8921b
estelasanchez7 <stelasv7@gmail.com>
2021-02-10 18:30:27 +0100

    NIF

---
+++ src/components/NIF.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+ <label :class="clases">{{label}}</label>
+  <input @keydown="comprobar" />
+</div>
+</template>
+
+<script>
+import {ref} from "vue"
+export default {
+  name: "NIF",
+  props: {
+    label: String,
+  },
+  setup() {
+    let clases=ref("label-bad")
+    const comprobar=(e)=>{
+        console.log(e.target.value)
+        let texto= e.target.value
+        let exp= /^\d{8}[a-zA-Z]{1}$/
+        if(exp.test(texto))
+            clases.value="label-ok"
+        else
+            clases.value="label-bad"
+    }
+
+    return {
+      comprobar,
+      clases
+    }
+  }
+}
+</script>
+
+<style>
+.label-bad {
+  padding: 5px;
+  color: aliceblue;
+  background: red;
+}
+.label-ok {
+  padding: 5px;
+  color: aliceblue;
+  background: green;
+}
+</style>
---
+++ src/views/Ejercicio1.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<h1>Ejercicio 1</h1>
+  <NIF label='DNI:'/>
+</div>
+  
+</template>
+
+<script>
+import NIF from '@/components/NIF.vue'
+export default {
+  components:{
+    NIF
+  }
+}
+</script>
+
+<style>
+
+</style>
